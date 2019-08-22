d40861f3e9603ee3f2b73765fcb36c8c34b52b79
Michael Pflueger <michael.pflueger@greenphire.com>
2019-08-22 15:23:35 -0400

    2nd commit

--- Find index.py
+++ Find index.py
@@ -1,10 +1,15 @@
-class Solution: 
-  def getRange(self, arr, target):
+class Solution:
+    def getRange(self, arr, target):
+        self.star = 0
+        self.end = 0
+        self.store= []
+        for x in range(0, len(arr)):
+            if arr[x] == target:
+                self.store.append(x)
 
-      for x in range(0, len(arr)):
-         if x == target:
-      
-      return "starting index = "
+        self.star = min(self.store)
+        self.end = max(self.store)
+        return ("starting index = {}, ending index = {}".format(self.star, self.end))
 
 arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8] 
 x = 2
